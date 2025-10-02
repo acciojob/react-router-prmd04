7d8b753ee24c41372fe0f9c99889a3b66e01c4de
pramod ade <prmdextracthere@gmail.com>
2025-10-02 14:16:01 +0530

    completed

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  )
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import { About } from "./About";
+import {Routes,Route,Link} from 'react-router-dom';
 
 const App = () => {
   return (
+    <>
     <div>
-        {/* Do not remove the main div */}
+      <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='about'>About</Link></li>
+    </ul>
     </div>
+    <Routes>
+      <Route path='/' element={<h1>Welcome to my website!</h1>}></Route>
+      <Route path='about' element={<About/>}></Route>
+    </Routes>
+   </>
   )
 }
 
